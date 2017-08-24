efde8670d0ac73414e72e5bf99e9078cccd49863
permotion <permotionk@gmail.com>
2017-08-24 16:05:13 -0300

    uart

---
+++ uart.X/build/default/production/main.o.d
@@ -0,0 +1,6 @@
+build/default/production/main.o:  \
+	/Users/permotion/Documents/PIC/MPLABXProjects/uart.X/main.asm  \
+	/Users/permotion/Documents/PIC/MPLABXProjects/uart.X/p12f1822.inc  \
+	/Users/permotion/Documents/PIC/MPLABXProjects/uart.X/HARDWARE_CFG.inc  \
+	/Users/permotion/Documents/PIC/MPLABXProjects/uart.X/UART_CFG.inc  \
+
